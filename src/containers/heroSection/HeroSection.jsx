c368c87b6a3fbf8acb49515e5a66ea49548d5464
a-humanthing <arunkrishnakt2002@gmail.com>
2023-02-16 18:59:23 +0530

    updated hero section

--- src/containers/heroSection/HeroSection.jsx
+++ src/containers/heroSection/HeroSection.jsx
@@ -1,31 +1,59 @@
+import { FacebookOutlined, Twitter, YouTube } from "@mui/icons-material"
 import React from "react"
 import { Button } from "react-bootstrap"
 import Navbar from "../../components/navbar/Navbar"
+// import.meta.env.BASE_URL
 import "./HeroSection.css"
 const HeroSection = () => {
   return (
     <>
       <div className="heroWrapper">
-        <div className="navContainer">
-          <Navbar />
-        </div>
-        <div className="heroContent">
-          <div className="heroLeft">
-            <div className="heroTitleDiv">
-              <span className="heroTitleText">ROOSTED COFEE FOR YOU</span>
+        <img src="/images/hero.jpg" alt="" className="heroBg" />
+        <div className="heroContentWrapper">
+          <div className="navContainer">
+            <Navbar />
+          </div>
+          <div className="heroContent">
+            <div className="heroLeft">
+              <div className="heroTitleDiv">
+                <span className="heroTitleText">ROOSTED COFFEE FOR YOU</span>
+              </div>
+              <div className="heroDescriptionDiv">
+                <span className="heroDescriptionText">
+                  The coffee if brewed by first roasting the green cofee beans
+                  over hot coals in a brazier. given an opportunity to sample
+                </span>
+              </div>
+              <div className="heroButtons mt-5">
+                <Button className="ms-2">Testy Coffee</Button>
+                <Button className="ms-2">Read More</Button>
+              </div>
+              <div className="socialMedia mt-5">
+                <div className="facebook ms-2">
+                  <FacebookOutlined />
+                  FACEBOOK
+                </div>
+                <div className="twitter ms-2">
+                  <Twitter />
+                  TWITTER{" "}
+                </div>
+                <div className="youtube ms-2">
+                  <YouTube />
+                  YOUTUBE
+                </div>
+              </div>
+              <div className="heroCup mt-5">
+                <img src="/images/cup.jpg" className="cupImage" alt="" />
+              </div>
             </div>
-            <div className="heroDescriptionDiv">
-              <span className="heroDescriptionText">
-                The coffee if brewed by first roasting the green cofee beans
-                over hot coals in a brazier. given an opportunity to sample
-              </span>
-            </div>
-            <div className="heroButtons mt-5">
-              <Button className="ms-2">Testy Coffee</Button>
-              <Button className="ms-2">Read More</Button>
+            <div className="heroRight">
+              <img
+                src="/images/arabic jug 1.svg"
+                alt=""
+                className="arabicJug"
+              />
             </div>
           </div>
-          <div className="heroRight">Mug Image</div>
         </div>
       </div>
     </>
