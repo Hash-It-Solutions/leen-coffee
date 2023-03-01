3d29a7ecfadcc3ba51e534efa9f198f3e45176e8
a-humanthing <arunkrishnakt2002@gmail.com>
2023-03-01 17:48:04 +0530

    updated with basic animations on menu component

--- src/containers/menu/Menu.jsx
+++ src/containers/menu/Menu.jsx
@@ -1,34 +1,63 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
+import { useInView } from "react-intersection-observer"
 import MenuItem from "../../components/menu/MenuItem"
 import "./Menu.css"
 const Menu = () => {
+  const { ref: myRef, inView: cupIsVisible } = useInView()
+  const { ref: menuRef, inView: menuItemsVisible } = useInView()
+  // const myRef = useRef()
+  // const menuRef = useRef()
+  // const [cupIsVisible, setCupIsVisible] = useState()
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver((entries) => {
+  //     const entry = entries[0]
+  //     setCupIsVisible(entry.isIntersecting)
+  //     console.log("entry", entry)
+  //   })
+  //   observer.observe(myRef.current)
+  // }, [])
   return (
     <>
       <div className="menuWrapper">
-        <img src="/images/menuCup.png" alt="" className="menuCup" />
+        <img
+          src="/images/menuCup.png"
+          alt=""
+          className={`menuCup${cupIsVisible ? ` animateCup` : ``}`}
+        />
         <div className="menuContentWrapper">
           <div className="row">
             <div className="col-6 menuHead text-center offset-3 mb-5">
-              <p className="yellowHead">MENU</p>
+              <p ref={myRef} className="yellowHead">
+                MENU
+              </p>
               <h1 className="headingText">CAFENA POPULAR MENU</h1>
             </div>
-            <div className="col-md-6 ">
-              <MenuItem />
-            </div>
-            <div className="col-md-6 ">
-              <MenuItem />
-            </div>
-            <div className="col-md-6 ">
-              <MenuItem />
-            </div>
-            <div className="col-md-6 ">
-              <MenuItem />
-            </div>
-            <div className="col-md-6 ">
-              <MenuItem />
-            </div>
-            <div className="col-md-6 ">
-              <MenuItem />
+            <div
+              ref={menuRef}
+              className={`menuItems ${
+                menuItemsVisible ? `animateMenu` : `staticMenu`
+              }`}
+            >
+              <div className="row">
+                <div className="col-md-6 ">
+                  <MenuItem />
+                </div>
+                <div className="col-md-6 ">
+                  <MenuItem />
+                </div>
+                <div className="col-md-6 ">
+                  <MenuItem />
+                </div>
+                <div className="col-md-6 ">
+                  <MenuItem />
+                </div>
+                <div className="col-md-6 ">
+                  <MenuItem />
+                </div>
+                <div className="col-md-6 ">
+                  <MenuItem />
+                </div>
+              </div>
             </div>
           </div>
         </div>
